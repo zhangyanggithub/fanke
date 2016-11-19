74f9a32679f75d5165adaecb253a6f85c43fdc1e
ZhangYang <1944952458@qq.com>
2016-11-19 15:35:41 +0800

    将menu原型中hide和show方法删除

--- commenFunction/menu.js
+++ commenFunction/menu.js
@@ -58,51 +58,6 @@ require(['zy.js'],function(zy) {
             }
             console.log(this.ulHeight);
         },
-      hide:function () {
-          var that = this;
-          for(var i = 1; i< zy_self.$('.menu-item').length;i++){
-              zy_self.$('.menu-item')[i].addEventListener('mouseleave',function () {
-                  var contentEle = this.getElementsByClassName('menu-son')[0];
-                  var contentUl = contentEle.getElementsByTagName('ul')[0];
-                  var a = that.ulHeight[i];
-                  var h = a;
-                  var timer = setInterval(function () {
-                      h -=3;
-                      if(h > 0){
-                          contentUl.style.cssText = 'height:' + h+'px;';
-                      }else{
-                          contentUl.style.height = 0;
-                          contentEle.style.visibility = 'hidden';
-                          clearInterval(timer);
-                      }
-                  },1)
-              });
-          }
-      },
-        show:function () {
-            var that = this;
-            for(var i = 1; i< zy_self.$('.menu-item').length;i++){
-                (function (i) {
-                    zy_self.$('.menu-item')[i].addEventListener('mouseenter',function () {
-                        var contentEle = this.getElementsByClassName('menu-son')[0];
-                        var contentUl = contentEle.getElementsByTagName('ul')[0];
-                        var a = that.ulHeight[i];
-                        var h = 0;
-                        contentUl.style.cssText = 'height:0';
-                        contentEle.style.cssText = 'visibility:visible;';
-                        var timer = setInterval(function () {
-                            h +=3;
-                            if(h <= a){
-                                contentUl.style.cssText = 'height:' + h+'px;';
-                            }else{
-                                contentUl.style.height = a;
-                                clearInterval(timer);
-                            }
-                        },1)
-                    });
-                })(i);
-            }
-        }
     };
     new menu();
 });
